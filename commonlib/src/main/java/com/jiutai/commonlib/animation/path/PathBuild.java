dbe5e02669761e07062c085293dedad767fdb390
429360705@qq.com <429360705@qq.com>
2019-03-11 14:41:20 +0800

    update .ignore

--- commonlib/src/main/java/com/jiutai/commonlib/animation/path/PathBuild.java
+++
@@ -1,8 +0,0 @@
-package com.jiutai.commonlib.animation.path;
-
-import com.jiutai.commonlib.animation.PathPoint;
-
-public abstract class PathBuild {
-
-    public abstract PathPoint evaluate(float t, PathPoint startValue, PathPoint endValue);
-}
--- commonlib/src/main/java/com/jiutai/commonlib/eventbus/ThreadMode.java
+++
@@ -1,5 +0,0 @@
-package com.jiutai.commonlib.eventbus;
-
-public enum ThreadMode {
-    MAIN, BACKGROUND
-}
